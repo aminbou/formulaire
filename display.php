016fb33c0c6c86bf28b116ed2c4e89823309e6c5
Amin <amination@hotmail.fr>
2018-11-11 15:50:42 +0100

    on ajoute le filtrege d'email

--- display.php
+++ display.php
@@ -3,5 +3,10 @@ $nom=$_POST["nom"];
 $prenom=$_POST["prenom"];
 $email=$_POST["email"];
 echo "Bonjour ".$nom." ".$prenom." <br>";
+if(filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
+    echo 'votre email est valide';
+}else{
+	echo 'vérifiez votre email';
+}
 
 ?>
